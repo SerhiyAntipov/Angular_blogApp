3da381010aa6a34aff8d91953fd0530da3a2f518
Serhiy Antipov <serhiy.antipov@gmail.com>
2022-01-05 17:31:54 +0200

    add dynymic StyleDirective

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 import {colors} from "@angular/cli/utilities/color";
 import {style} from "@angular/animations";
 
@@ -10,7 +10,17 @@ export class StyleDirective {
     constructor(private elRef: ElementRef, private renderer: Renderer2) {
         // console.log(elRef)
         // this.elRef.nativeElement.style.color = 'red'
-        this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+        // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+    }
+    @HostListener('click', ['$event']) onClick(event){
+        console.log(event)
     }
 
+    @HostListener('mouseenter') onEnter(){
+        this.renderer.setStyle( this.elRef.nativeElement, 'color', 'blue')
+    }
+
+    @HostListener('mouseleave') onLeave(){
+        this.renderer.setStyle( this.elRef.nativeElement, 'color', null)
+    }
 }
