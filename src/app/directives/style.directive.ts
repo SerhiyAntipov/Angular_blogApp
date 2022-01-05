72b02e68974b077636768a7a9f0825d7d0bc6789
Serhiy Antipov <serhiy.antipov@gmail.com>
2022-01-05 18:12:18 +0200

    add HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from "@angular/core";
 import {colors} from "@angular/cli/utilities/color";
 import {style} from "@angular/animations";
 
@@ -10,6 +10,8 @@ export class StyleDirective {
     @Input() fontWeight: string = 'normal'
     @Input() dStyles: {border?: '1px solid red', borderRadius?: '5px'}
 
+    @HostBinding('style.color') elColor = null
+
     constructor(private elRef: ElementRef, private renderer: Renderer2) {
         // console.log(elRef)
         // this.elRef.nativeElement.style.color = 'red'
@@ -20,14 +22,16 @@ export class StyleDirective {
     }
 
     @HostListener('mouseenter') onEnter(){
-        this.renderer.setStyle( this.elRef.nativeElement, 'color', this.color)
-        this.renderer.setStyle( this.elRef.nativeElement, 'border', this.dStyles.border)
-        this.renderer.setStyle( this.elRef.nativeElement, 'borderRadius', this.dStyles.borderRadius)
+        this.elColor = this.color
+        // this.renderer.setStyle( this.elRef.nativeElement, 'color', this.color)
+        // this.renderer.setStyle( this.elRef.nativeElement, 'border', this.dStyles.border)
+        // this.renderer.setStyle( this.elRef.nativeElement, 'borderRadius', this.dStyles.borderRadius)
     }
 
     @HostListener('mouseleave') onLeave(){
-        this.renderer.setStyle( this.elRef.nativeElement, 'color', null)
-        this.renderer.setStyle( this.elRef.nativeElement, 'border', null)
-        this.renderer.setStyle( this.elRef.nativeElement, 'borderRadius', null)
+        this.elColor = null
+        // this.renderer.setStyle( this.elRef.nativeElement, 'color', null)
+        // this.renderer.setStyle( this.elRef.nativeElement, 'border', null)
+        // this.renderer.setStyle( this.elRef.nativeElement, 'borderRadius', null)
     }
 }
